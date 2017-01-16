da78c85367cc21789ff2475b6d89ca45c1bae8a0
root <rudy.artha@bankandara.co.id>
2017-01-16 17:07:26 +0800

    update auth server with inmemory token and hardcoded user credential

---
+++ src/main/java/com/server/app/WebSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.server.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(auth);
+		auth
+			.inMemoryAuthentication()
+			.withUser("rudy").password("123").roles("ADMIN").and()
+			.withUser("surya").password("123").roles("STAFF");
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
+	}
+}
