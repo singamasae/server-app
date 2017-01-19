2b121864e5370742503c8501e17c9da304d8aec7
rudy <rudy@rudy-Andara-PC>
2017-01-19 17:05:34 +0800

    update default token store to JWT

--- src/main/java/com/app/server/auth/OAuth2AuthorizationServerConfiguration.java
+++ src/main/java/com/app/server/auth/OAuth2AuthorizationServerConfiguration.java
@@ -2,6 +2,7 @@ package com.app.server.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -9,53 +10,55 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
-	private static final String RESOURCE_ID = "OAuth2RestApp";
 	private static final String CLIENT_ID = "clientapp";
 	private static final String CLIENT_SECRET = "123456";
-	
+
 	@Autowired
 	@Qualifier("authenticationManagerBean")
 	private AuthenticationManager authenticationManager;
-	
+
 	@Autowired
 	@Qualifier("userDetailsService")
 	private MyUserDetailsService userDetailsService;
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		// TODO Auto-generated method stub		
-		endpoints
-			.tokenStore(new InMemoryTokenStore())
-			.authenticationManager(authenticationManager)
-			.userDetailsService(userDetailsService);
+		// TODO Auto-generated method stub
+		endpoints.tokenStore(tokenStore()).accessTokenConverter(accessTokenConverter())
+				.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
 	}
-
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		// TODO Auto-generated method stub
-		security
-			.tokenKeyAccess("permitAll()")
-			.checkTokenAccess("isAuthenticated()");		
+		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
 	}
-	
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		// TODO Auto-generated method stub		
-		clients
-			.inMemory()
-			.withClient(CLIENT_ID)			
-			.authorizedGrantTypes("password", "refresh_token")			
-			.scopes("read", "write")
-			.resourceIds(RESOURCE_ID)
-			.secret(CLIENT_SECRET);
+		// TODO Auto-generated method stub
+		clients.inMemory().withClient(CLIENT_ID).authorizedGrantTypes("password", "refresh_token")
+				.scopes("read", "write").secret(CLIENT_SECRET);
 
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		converter.setSigningKey("pekaryanNakB4l1");
+		return converter;
 	}
 
 }
--- src/main/java/com/app/server/resource/OAuth2ResourceServerConfiguration.java
+++ src/main/java/com/app/server/resource/OAuth2ResourceServerConfiguration.java
@@ -1,28 +1,40 @@
 package com.app.server.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableResourceServer
 public class OAuth2ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-	private static final String RESOURCE_ID = "OAuth2RestApp";
-	
+
+	@Autowired
+	private TokenStore tokenStore;
+
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-		// TODO Auto-generated method stub		
-		resources.resourceId(RESOURCE_ID);
+		// TODO Auto-generated method stub
+		resources.tokenServices(tokenServices());
 	}
-	
+
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
-		http
-			.authorizeRequests()
-				.antMatchers("/api/test").permitAll()
-				.anyRequest().authenticated();
+		http.authorizeRequests().antMatchers("/api/test").permitAll().anyRequest().authenticated();
+	}
+
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore);
+		return defaultTokenServices;
 	}
 }
